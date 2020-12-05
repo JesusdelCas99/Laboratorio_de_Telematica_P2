e9e9a899745dfeffe15a3e215ee2bd187212ed53
Serfermor <72385473+Serfermor@users.noreply.github.com>
2020-12-05 17:27:48 +0100

    Paso5
    Base de datos terminada, inicio optimización, resuelvo errores en pasos anteriores.

--- App_Design.py
+++ App_Design.py
@@ -19,12 +19,21 @@ from Funciones import *
   
 ########################################################################### 
     
+def Paso0():
+    
+    dlg5.close()
+    dlg.show()
+
+
+
 def Paso2(): 
     
+    dlg.close()
     dlg2.show()   
     
     global Codecs_parametros
     global Codecs
+    global MOS
     
     MOS=dlg.comboBox_2.currentText()
     
@@ -39,16 +48,16 @@ def Paso2():
     else:
         MOS=1
 
-    #Seleccionamos de la base de datos los codecs que tengan MOS mayor igual
-    #al planteado por el usuario
-    conn = sql.connect('DataBaseMOS.db')
+    #Seleccionamos de la base de datos los codecs que tengan MOS que satisfaga 
+    #el QoE introducido por el usuario
+    conn = sql.connect('DataBase.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM Codecs_DataBase where MOS >=' + str(MOS))
     records = cursor.fetchall()
     
     
     
-    Codecs_parametros=np.zeros((len(records),9))
+    Codecs_parametros=np.zeros((len(records),10))
     Codecs=list()
     
     #Agrupo todos los codecs en una lista y sus parámetros en una matriz
@@ -56,47 +65,71 @@ def Paso2():
     
     for raw in records:        
           Codecs.append(raw[0])
-          for j in range(1,10):             
+          for j in range(1,11):             
               Codecs_parametros[z,j-1]=float(raw[j])
           z+=1
             
+         
+    dlg2.lineEdit_2.setText(Codecs[0])
+    dlg2.lineEdit_2.setReadOnly(True)        
+          
     dlg2.lineEdit.setText(dlg.comboBox_2.currentText())
     dlg2.lineEdit.setReadOnly(True)   
 ##########################################################################
 
 def Paso3():
     
-    #Mostramos la ventana referente al paso 3
-    dlg3.show() 
-    
+     
     global Retardo_total
     global R_final
     global dim #Numero de Codecs seleccionados
+
     
-    Jitter=float(dlg.lineEdit_11.text())*10^(-3)
-    Retardo_red=float(dlg.lineEdit_10.text())*10^(-3)
-    Retardo_VoIp=dlg.comboBox.text()
-    Retardo_G114=dlg.comboBox_3.text()
+    Jitter=float(dlg.lineEdit_11.text())*1e-3
+    Retardo_red=float(dlg.lineEdit_10.text())*1e-3
+    Retardo_VoIp=dlg.comboBox.currentText()
+    Retardo_G114=dlg.comboBox_3.currentText()
     
                        
-    if Retardo_VoIp=='aceptable':
-        Retardo_VoIp=150*10^(-3)
-    elif Retardo_VoIp=='moderado':
-        Retardo_VoIp=400*10^(-3)
+    if Retardo_VoIp=='Aceptable':
+        Retardo_VoIp=150*1e-3
+    elif Retardo_VoIp=='Moderado':
+        Retardo_VoIp=400*1e-3
     else:
         Retardo_VoIp=1
         
     
     if Retardo_G114=='Muy satisfecho':
-        Retardo_G114=200*10^(-3)
-    elif Retardo_G114=='satisfecho':
-        Retardo_G114=287.5*10^(-3)
+        Retardo_G114=200*1e-3
+    elif Retardo_G114=='Satisfecho':
+        Retardo_G114=287.5*1e-3
     elif Retardo_G114=='Alguno insatisfecho':
-        Retardo_G114=387.5*10^(-3)
+        Retardo_G114=387.5*1e-3
     elif Retardo_G114=='Mucho insatisfecho':
-        Retardo_G114=550*10^(-3)
+        Retardo_G114=550*1e-3
     elif Retardo_G114=='Casi todos insatisfechos':
         Retardo_G114=1
+        
+                
+    #Mostramos la ventana referente al paso 3
+    dlg2.close()
+    dlg3.show() 
+    
+    
+    dlg3.lineEdit.setText(Codecs[0])
+    dlg3.lineEdit.setReadOnly(True)
+    
+    dlg3.lineEdit_11.setText(dlg.lineEdit_11.text())
+    dlg3.lineEdit_11.setReadOnly(True) 
+    
+    dlg3.lineEdit_10.setText(dlg.lineEdit_10.text())
+    dlg3.lineEdit_10.setReadOnly(True) 
+    
+    dlg3.lineEdit_12.setText(str(Retardo_G114*1e3) + " ms")
+    dlg3.lineEdit_12.setReadOnly(True)
+    
+    dlg3.lineEdit_13.setText(str(Retardo_VoIp*1e3) + " ms")
+    dlg3.lineEdit_13.setReadOnly(True)
     
     Retardo_total=min(Retardo_G114,Retardo_VoIp)
     
@@ -115,10 +148,10 @@ def Paso3():
     for i in range(dim):
         
         #Cargamos el retardo de look ahead de la base de datos
-        n_look_ahead=3*10^(-3)
+        n_look_ahead=Codecs_parametros[i][9]*1e-3
         
         #Retraso de paquetizacion
-        VPS=Codecs_parametros[i][4]/((Codecs_parametros[i][0])*10^(3))
+        VPS=Codecs_parametros[i][4]/((Codecs_parametros[i][0])*1e-3)
         R_final[i]=VPS
         
         #Retraso algoritmico
@@ -151,12 +184,16 @@ def Paso3():
             
             #Numero de paquetes a almacenar por el buffer antijitter
             Storage_P[i]=int((1.5*Jitter)/VPS)
+            
+    dlg3.lineEdit_2.setText(str(R_final[0]) + " ms")
+    dlg3.lineEdit_2.setReadOnly(True)
                                     
            
 # Calculo de GoS número de líneas en una ventana adicional #################
 def Paso4():
     
     #Mostramos la ventana referente al paso 4
+    dlg3.close()
     dlg4.show()    
     
     dlg4.lineEdit_2.setText(dlg.lineEdit_9.text()) 
@@ -186,18 +223,32 @@ def Paso4():
 def Paso5():
     
     #Mostramos la ventana referente al paso 5
+    dlg4.close()
     dlg5.show()
+    
+    
+    dlg5.pushButton_2.hide()
+    dlg5.pushButton_3.hide()
+    dlg5.label_6.hide()
+    dlg5.label_7.hide()
+    
     
     #Colocamos el ancho de banda con variable global
     global Bandwidth
-
+    
+    #Variable para controlar si es necesario Paso6
+    Nocumple=0
+    
     #Calculamos el ancho de banda global
-    Bandwidth=4*10^(6)  #Esto es algo que se deberia de dar no? 
+    Bandwidth=(float(dlg.lineEdit_3.text()))*1e6
     
     #Extraemos de la GUI los parametros de cabeceras del paquete
-    P_Enlace=dlg.comboBox_5.text()
-    P_Tuneles=dlg.comboBox_4.text()
+    P_Enlace=dlg.comboBox_4.currentText()
+    P_Tuneles=dlg.comboBox_5.currentText()
     B_W_percentage=float(dlg.lineEdit_7.text()) 
+    
+                         
+  
     
     if P_Tuneles=='MPLS':
         P_Tuneles=4
@@ -219,13 +270,15 @@ def Paso5():
         P_Enlace=24
     elif P_Enlace=='Ethernet': 
         P_Enlace=18
+        
+        
     
     #Calculamos la longitud de la cabecera para RTP y cRTP que sera 
     #la misma para todos los Codecs. Para el caso de cRTP consideramos
     #que comprime IP/UDP/RTP a un total de 4B. Las colas asociadas al checksum
     #siguen presentes en cRTP
-    L_RTP_Cabeceras=P_Enlace+P_Tuneles+20+8+12+4
-    L_cRTP_Cabeceras=P_Enlace+P_Tuneles+4+4
+    L_RTP_Cabeceras=int(P_Enlace) + int(P_Tuneles) + 20 + 8 + 12 + 4
+    L_cRTP_Cabeceras=int(P_Enlace) + int(P_Tuneles) + 4 + 4
     
     #Calculamos el ancho de banda para los dos posibles casos: RTP y cRTP
     #CASO 1: RTP: Tendremos en cuenta el checksum (4bytes)
@@ -234,7 +287,7 @@ def Paso5():
     for i in range(dim):
         
         L_RTP=8*(Codecs_parametros[i][4]+L_RTP_Cabeceras)
-        PPS=(Codecs_parametros[i][0]*10^(3))/(8*Codecs_parametros[i][4])
+        PPS=(Codecs_parametros[i][0]*1e3)/(8*Codecs_parametros[i][4])
         Bw_llamada=L_RTP*PPS*(1+B_W_percentage)
         BW_SP_RTP=N_lineas*Bw_llamada
         
@@ -255,10 +308,58 @@ def Paso5():
         else:
             BW_SP[i]=BW_SP_cRTP #Nos quedamos con cRTP
             
-        #FALTA SACAR POR PANTALLA TODOS LOS DATOS Y VER COMO DISEÑAMOS
-        #CADA UNA DE LAS VENTANAS PARA SACAR LOS DATOS
             
+            
+            
+    #Ahora mismo mostramos como resultado el Primer Codec que cumple el MOS
+            
+    dlg5.lineEdit.setText(Codecs[0])
+    dlg5.lineEdit.setReadOnly(True)
+    
+    
+    if Codecs_parametros[0][3]<MOS:
+        dlg5.lineEdit_2.setText("No cumple")
+        Nocumple=1
+    else:
+        dlg5.lineEdit_2.setText("Cumple")
+    dlg5.lineEdit_2.setReadOnly(True)
+    
+    
+    if R_final[0]>Retardo_total:
+        dlg5.lineEdit_3.setText("No cumple")
+        Nocumple=1
+    else:
+        dlg5.lineEdit_3.setText("Cumple")
+    dlg5.lineEdit_3.setReadOnly(True)
+    
+    
+    #Tal y como está planteado no se me ocurre forma de que se supere la
+    #probabilidad de bloqueo que introduce el usuario
+    dlg5.lineEdit_4.setText("Cumple")
+    dlg5.lineEdit_4.setReadOnly(True)        
+    
+    
+    if BW_SP[0] > Bandwidth:
+        dlg5.lineEdit_5.setText("No cumple")
+        Nocumple=1
+    else:
+        dlg5.lineEdit_5.setText("Cumple")
+    dlg5.lineEdit_5.setReadOnly(True)
         
+
+    if Nocumple == 1:
+        dlg5.pushButton_2.show()
+        dlg5.pushButton_3.show()
+        dlg5.label_6.show()
+        dlg5.label_7.show()
+    
+
+def Paso6():
+    
+    dlg5.close()
+    dlg6.show()
+    
+    dlg6.comboBox.addItems(Codecs)
            
 ###########################################################################
 ################## PROGRAMACION BACK-END ##################################
@@ -270,24 +371,36 @@ app=QtWidgets.QApplication([])
 #Cargamos el archivo '.ui' diseñado mediante Qt Designer
 dlg=uic.loadUi("VoIp_SoftLab.ui")
 
-
-#dlg.spinBox.setVisible(False)
     
 dlg2=uic.loadUi("Paso2.ui")
+dlg3=uic.loadUi("Paso3.ui")
 dlg4=uic.loadUi("Paso4.ui")
-#Falta crear la nueva ventana para el paso 3 y 5
+dlg5=uic.loadUi("Paso5.ui")
+dlg6=uic.loadUi("Paso6a.ui")
 
 
 #Placeholders, ¿usar en otros campos?
 dlg.lineEdit_5.setPlaceholderText("Probabilidad entre 0-1")
 dlg.lineEdit_9.setPlaceholderText("0-1")
 
+#dlg.spinBox.hide()
 
 dlg.pushButton.clicked.connect(Paso2) # Pops new window when pushed
 
 
-
 dlg2.pushButton.clicked.connect(Paso3)
+
+
+dlg3.pushButton.clicked.connect(Paso4)
+
+
+dlg4.pushButton.clicked.connect(Paso5)
+
+
+dlg5.pushButton_3.clicked.connect(Paso0)
+
+
+dlg5.pushButton_2.clicked.connect(Paso6)
 
 
 
---
+++ Pruebas.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 17:13:12 2020
+
+@author: serfe
+"""
+
+
+a=Codecs_parametros[1][0]
